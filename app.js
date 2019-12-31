bd029b4eaca4a21aa20450cb3fe4eeddac07b41f
abanglaile <abanglaile@users.noreply.github.com>
2019-12-31 17:25:37 +0800

    新增登录鉴权流程

--- app.js
+++ app.js
@@ -9,7 +9,29 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log('res.code:',res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url: this.globalData.server_url + '/get_xcx_auth',
+          data: {
+            code: res.code
+          },
+          // method: 'POST',
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            console.log('openid:',JSON.stringify(res.data.data.openid)) //获取openid 
+            if (res.data.code == 0) {
+              wx.setStorage({
+                key: "token",
+                data: res.data.data.token,
+              });
+              this.globalData.openid = res.data.data.openid;
+              // console.log("openid:",res.data.data.openid);
+            }
+          }
+        }) 
       }
     })
     // 获取用户信息
@@ -21,7 +43,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              // console.log('res.userInfo:', res.userInfo)
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -29,11 +51,18 @@ App({
               }
             }
           })
+        } else{
+          wx.reLaunch({
+            url: '/pages/authorize/authorize',
+          })
         }
       }
     })
   },
   globalData: {
-    userInfo: null
+    openid: null,
+    userid: null,
+    userInfo: null,
+    server_url: "https://www.zhiqiu.pro/api"
   }
 })
