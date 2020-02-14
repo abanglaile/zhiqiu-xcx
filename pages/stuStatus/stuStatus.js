8da7ad781a3555f7aa3842d0da6a34af348877f5
edique <278185720@qq.com>
2020-02-14 16:43:38 +0800

    课程页面更新

--- pages/stuStatus/stuStatus.js
+++ pages/stuStatus/stuStatus.js
@@ -53,58 +53,12 @@ Page({
         "course_label_name": "英语"
       },
     ],
-    all_list: [
-      { txt: '已签到', num: 1 },
-      { txt: '未签到', num: 0 },
-    ],
-    all_states: false,
-    selectd: 0,
-    testCourseJson:[
-      { txt:'语文', id:0,selectd:false},
-      { txt: '数学', id: 1, selectd: false},
-      { txt: '英语', id: 2, selectd: false},
-      { txt: '声乐', id: 3, selectd: false }
-    ],
-    lebel_list:[
-      { txt: '函授', num: 0 },
-      {txt:'导学',num:1}
-    ],
-    select_label_draw: false,
-    lebel_selectd:0
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      loading: true
-    })
-    const start_end_time = this.getStartEndTime(this.data.select_time);
-    let filter_option = {
-      start_time: start_end_time.start_time,
-      end_time: start_end_time.end_time,
-      course_label_list: this.data.course_list.filter(c => c.selected),
-      label_id_list: this.data.label_list.filter(l => l.selected),
-      is_sign: this.data.select_sign == '不限' ? -1 : this.data.select_sign == '已上课' ? 1 : 0
-    }
-    console.log(filter_option);
-    // wx.request({
-    //   url: 'test.php', //仅为示例，并非真实的接口地址
-    //   data: {
-    //     student_id: this.app.global.student_id,
-    //     filter_option: filter_option
-    //   },
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   success(res) {
-    //     this.setData({
-    //       loading: false,
-    //       student_lesson: res.data
-    //     })
-    //     console.log(res.data)
-    //   }
-    // })
+    getStudentLesson();
   },
   
   /**
@@ -155,78 +109,6 @@ Page({
   onShareAppMessage: function () {
 
   },
-  toggleDrawerShow: function () {
-    this.setData({
-      showDrawer: !this.data.showDrawer
-    })
-  },
-
-  startTimeChange: function (e) {
-    this.setData({
-      startTime: e.detail.value
-    })
-  },
-  startDateChange: function (e) {
-    this.setData({
-      startDate: e.detail.value
-    })
-  },
-  endTimeChange: function (e) {
-    this.setData({
-      endTime: e.detail.value
-    })
-  },
-  endDateChange: function (e) {
-    this.setData({
-      endDate: e.detail.value
-    })
-  },
-  teacherNameChange: function (e) {
-    this.setData({
-      teacherNameIndex: e.detail.value
-    })
-  },
-  groupNameChange: function (e) {
-    this.setData({
-      groupNameIndex: e.detail.value
-    })
-  },
-  courseNameChange: function (e) {
-    this.setData({
-      courseNameIndex: e.detail.value
-    })
-  },
-  tabChange: function (e) {
-    this.setData({
-      currentTab: e.detail.key
-    })
-  },
-  changeBox: function (e) {
-    this.setData({
-      all_states: !this.data.all_states,
-      
-    })
-  },
-  select: function(e) {
-    this.setData({
-      selectd: e.target.dataset['index']
-    }) 
-  },
-  changeTimeBox: function(e){
-    this.setData({
-      select_time_draw: !this.data.select_time_draw
-    })
-  },
-  changeCourseBox: function(e){
-    this.setData({
-      select_course_draw: !this.data.select_course_draw
-    })
-  },
-
-
-
-
-
   selectCourse: function(e){
     let index = e.target.dataset['index']
     this.data.testCourseJson[index].selectd = !this.data.testCourseJson[index].selectd
@@ -247,7 +129,7 @@ Page({
 
   /**********/
   selectTab: function (e) {
-    console.log(this.data.filter_visible)
+    console.log(e)
     this.setData({
       filter_visible: !this.data.filter_visible,
       select_tab_num: e.target.dataset.num
@@ -272,6 +154,38 @@ Page({
           end_time: null
         }
     }
+  },
+
+  getStudentLesson(){
+    this.setData({
+      loading: true
+    })
+    const start_end_time = this.getStartEndTime(this.data.select_time);
+    let filter_option = {
+      start_time: start_end_time.start_time,
+      end_time: start_end_time.end_time,
+      course_label_list: this.data.course_list.filter(c => c.selected),
+      label_id_list: this.data.label_list.filter(l => l.selected),
+      is_sign: this.data.select_sign == '不限' ? -1 : this.data.select_sign == '已上课' ? 1 : 0
+    }
+    console.log(filter_option);
+    // wx.request({
+    //   url: 'test.php', //仅为示例，并非真实的接口地址
+    //   data: {
+    //     student_id: this.app.global.student_id,
+    //     filter_option: filter_option
+    //   },
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success(res) {
+    //     this.setData({
+    //       loading: false,
+    //       student_lesson: res.data
+    //     })
+    //     console.log(res.data)
+    //   }
+    // })
   },
 
   formatLessonTime(start_time, end_time) {
@@ -322,7 +236,12 @@ Page({
   },
 
   onActionClose(e) {
-    console.log("sssss");
+    this.setData({
+      filter_visible: false
+    })
+  },
+
+  onClose(e){
     this.setData({
       filter_visible: false
     })
@@ -332,6 +251,7 @@ Page({
     this.setData({
       filter_visible: false
     })
+    getStudentLesson();
   },
 
   onReset(e) {
